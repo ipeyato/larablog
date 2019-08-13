a7d239ebc2b181e2cd68de2afc0c97f6d859f73b
ipeyato <ipeyato@gmail.com>
2019-08-13 22:25:46 +0700

    Edit Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -100,7 +100,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
     }
 
     /**
@@ -112,7 +113,51 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'name' => 'required|unique:categories',
+            'image' => 'required|mimes:jpeg,bmp,png,jpg',
+        ]);
+        // get from image
+        $image = $request->file('image');
+        $slug = str_slug($request->name);
+        $category = Category::find($id);
+        if (isset($image)) {
+            // make unique name for image
+            $currentDate = Carbon::now()->toDateString();
+            $imageName = $slug.'_'.$currentDate.'_'.uniqid().'.'.$image->getClientOriginalExtension();
+            // check category dir is exists
+            if (!Storage::disk('public')->exists('category')) {
+                Storage::disk('public')->makeDirectory('category');
+            }
+            // delete old image
+            if (Storage::disk('public')->exists('category/'.$category->image)) {
+                Storage::disk('public')->delete('category/'.$category->image);
+            }
+            // resize image for category and upload
+            $categoryImage = Image::make($image)->resize(1600,479)->save($imageName);
+            Storage::disk('public')->put('category/'.$imageName, $categoryImage);
+
+            // check category slider dir is exists
+            if (!Storage::disk('public')->exists('category/slider')) {
+                Storage::disk('public')->makeDirectory('category/slider');
+            }
+            // delete old slider image
+            if (Storage::disk('public')->exists('category/slider/'.$category->image)) {
+                Storage::disk('public')->delete('category/slider/'.$category->image);
+            }
+            // resize image for category slider and upload
+            $sliderImage = Image::make($image)->resize(500,333)->save($imageName);
+            Storage::disk('public')->put('category/slider/'.$imageName, $sliderImage);
+        } else {
+            $imageName = $category->image;
+        }
+
+        $category->name = $request->name;
+        $category->slug = $slug;
+        $category->image = $imageName;
+        $category->update();
+        Toastr::success('Category Successfuly Updated', 'Success');
+        return redirect()->route('admin.category.index');
     }
 
     /**
